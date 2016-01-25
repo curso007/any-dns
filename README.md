c8201f7901ac5aab9b3d842eb9bae578c08b8b20
Rogier Slag <rogier.slag@gmail.com>
2016-01-25 19:00:52 +0100

    Readme and code improvements

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![inventid logo](https://cdn.inventid.nl/assets/logo-horizontally-ba8ae38ab1f53863fa4e99b977eaa1c7.png)](http://opensource.inventid.nl)
+
 # Any DNS
 
 [![Docker downloads](https://img.shields.io/docker/pulls/inventid/any-dns.svg)](https://registry.hub.docker.com/u/inventid/any-dns/)
@@ -12,7 +14,21 @@ Therefore we introduce any-dns.
 Simply pick any IP, replace either the `:` or the `.` with dashes (`-`), and fire your request to `10-0-0-2.v4.inventid.me`.
 This will return the ip address `10.0.0.2`.
 
-IPv6 is also supported by the application, but not publicly enabled.
+IPv6 is also supported by the application, but not enabled (and it seems it's unstable so far).
+
+## Environment variables
+
+You can simply use the Docker container.
+To specify some configuration, use environment variables.
+The following vars are available:
+
+| Variable | Description | Default
+|---|---|---|
+| `DOMAIN` | The main domain to serve under. The service will pick `v4.$DOMAIN` and `v6.domain` for ipv4 and ipv6 respectively | `inventid.me` |
+| `V4` | Set to `true` or `false` to enable or disable ipv4 resolution | `true` |
+| `V6` | Set to `true` or `false` to enable or disable ipv6 resolution | `false` |
+| `PORT` | Set to the port number to function under (note that it should externally be accessible under port 53 to allow resolution | `5353` |
+| `IP` | The IP address to bind to | `0.0.0.0` (all ips) |
 
 ## Contributing
 
--- index.js
+++ index.js
@@ -5,15 +5,36 @@ const domain = process.env.DOMAIN || 'inventid.me';
 const v4 = `.v4.${domain}`;
 const v6 = `.v6.${domain}`;
 const ipv4 = Boolean(process.env.V4) || true;
-const ipv6 = Boolean(process.env.V4) || false;
+const ipv6 = Boolean(process.env.V6) || false;
 const port = process.env.PORT || 5353;
 const ip = process.env.IP || '0.0.0.0';
 
 const ONE_DAY_IN_SECS = 84600;
 
+function logServerInfo() {
+  console.log(`Server running at ${ip}:${port} using domain ${domain}`);
+  if (ipv4) {
+    console.log(`IPv4 is enabled on ${v4}`);
+  } else {
+    console.log(`IPv4 is NOT enabled`);
+  }
+  if (ipv6) {
+    console.log(`IPv6 is enabled on ${v6}`);
+  } else {
+    console.log(`IPv6 is NOT enabled`);
+  }
+}
+
+function isIpv4(hostname) {
+  return hostname.endsWith(v4);
+}
+
+function isIpv6(hostname) {
+  return hostname.endsWith(v6);
+}
+
 dnsd
-  .createServer(function (req, res) {
-    //console.log(res);
+  .createServer((req, res) => {
     try {
       const question = req.question[0];
       const hostname = question.name;
@@ -25,40 +46,26 @@ dnsd
 
       if (ipv4 && isIpv4(hostname)) {
         // Handle ipv4
-        const queriedPart = hostname.replace(v4, '');
-
-        const returnIp = queriedPart.replace(/-/g, '.');
+        const returnIp = hostname.replace(v4, '').replace(/-/g, '.');
         if (!ipTools.isV4Format(returnIp)) {
           console.warn(`Could not successfully parse hostname ${hostname} to ipv4`);
           return res.end();
         }
-        res.answer.push({name: hostname, type: 'A', data: returnIp, 'ttl': ONE_DAY_IN_SECS});
+        res.answer.push({name: hostname, type: 'A', data: returnIp, ttl: ONE_DAY_IN_SECS});
       } else if (ipv6 && isIpv6(hostname)) {
         // Handle ipv6
-        const queriedPart = hostname.replace(v6, '');
-        const returnIp = queriedPart.replace(/-/g, ':');
+        const returnIp = hostname.replace(v6, '').replace(/-/g, ':');
 
         if (!ipTools.isV6Format(returnIp)) {
           console.warn(`Could not successfully parse hostname ${hostname} to ipv6`);
           return res.end();
         }
-        res.answer.push({name: hostname, type: 'AAAA', data: returnIp, 'ttl': ONE_DAY_IN_SECS});
+        res.answer.push({name: hostname, type: 'AAAA', data: returnIp, ttl: ONE_DAY_IN_SECS});
       }
     } catch (e) {
-      console.warn(`An error occurred while replying: ${e}`);
+      // In case something goes horribly wrong while sending UDP packets
+      console.error(`An error occurred while replying: ${e}`);
     }
     res.end();
   }).zone(v4, v6)
-  .listen(port, ip, function () {
-    console.log(`Server running at ${ip}:${port} using domains ${domain}`);
-    if (ipv4) console.log(`IPv4 is supported on ${v4}`);
-    if (ipv6) console.log(`IPv6 is supported on ${v6}`);
-  });
-
-function isIpv4(hostname) {
-  return hostname.endsWith(v4);
-}
-
-function isIpv6(hostname) {
-  return hostname.endsWith(v6);
-}
+  .listen(port, ip, logServerInfo);
